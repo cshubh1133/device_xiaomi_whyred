7c92493d4067cbe161494b04357b79a297d7807b
arnavpuranik <puranikarnav@gmail.com>
2021-04-15 06:44:41 +0000

    whyred: Adapted to Fluid
    Signed-off-by: arnavpuranik <puranikarnav@gmail.com>
    
    whyred: Adapted to Fluid
    
    Signed-off-by: arnavpuranik <puranikarnav@gmail.com>

--- fluid_whyred.mk
+++ fluid_whyred.mk
@@ -29,14 +29,13 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 $(call inherit-product-if-exist, vendor/MiuiCamera/config.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/fluid/config/common.mk)
 
-DEVICE_MAINTAINER := Sreekanth
-PRODUCT_NAME := arrow_whyred
+PRODUCT_NAME := fluid_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Redmi Note 5
+PRODUCT_MODEL := Redmi Note 5 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -55,3 +54,8 @@ IS_PHONE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SCREEN_DENSITY := 440
+
+# Fluid stuff
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=Arnav \
+  ro.fluid.cpu=SD636
